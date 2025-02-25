e62456a24c0894a10fb0a6dea1a60f23875a661b
Edy S. <edysoto56@gmail.com>
2025-02-24 23:39:11 -0600

    Display questions and capture answers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import quizLogo from '../assets/quiz-logo.png';
+import Quiz from './Quiz';
 
 function Header() {
     return (
+        <>
         <header>
             <img src={quizLogo} alt="Quiz App logo" />
             <h1>My React JS Quiz App</h1>
         </header>
+        <Quiz />
+        </>
     );
 }
 
